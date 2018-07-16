9d4da3b55d3b353b5744f214ba61c626309f7c82
zorochimaru <rasim.karimli@gmail.com>
2018-07-16 23:07:28 +0400

    animations, preloader done

--- js/common.js
+++ js/common.js
@@ -1,7 +1,14 @@
-$('.fotorama').fotorama({
-	fit:'cover',
-	width: '100%',
-  ratio: 16/7,
-  loop:true,
-	autoplay:true,
+//PRELOADER
+$(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({ 'overflow': 'visible' });
 });
+	$('.fotorama').fotorama({
+		fit:'cover',
+		width: '100%',
+	  ratio: 16/7,
+	  loop:true,
+		autoplay:true,
+	});
+	new WOW().init();
